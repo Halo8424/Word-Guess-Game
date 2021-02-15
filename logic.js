c71f18a7dfc259a38b68bd3ab687c3474dfee863
Richard.C <r.conde@live.com>
2021-02-15 08:31:13 -0500

    global variables list

---
+++ logic.js
@@ -0,0 +1,14 @@
+// Global variables all functions can access
+
+// List of words to be guessed
+let wordList = ["red","orange","purple","black"];
+
+let wins = 0;
+let losses = 0;
+let remainingGuesses = 10;
+let selectedWord = "";
+let lettersInSelectedWord =[];
+let numBlanks = 0;
+let blanksAndCorrectLetters = [];
+let wrongGuesses = [];
+
